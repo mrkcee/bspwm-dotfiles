3bfde68ecf8168b529f1191725ac7b1cd2ff97c2
mrkcee <markz_a@yahoo.com>
2022-06-25 15:51:25 +0800

    Optimized zsh script

--- backup.zsh
+++ backup.zsh
@@ -1,45 +1,23 @@
 #!/bin/zsh -e
 
-echo 'Backing up bspwm...'
-if [[ -d ./bspwm ]]; then
-  rm -rf ./bspwm
-fi
-cp -r ~/.config/bspwm .
+config_files=(
+	alacritty
+	bspwm
+	dunst
+	picom
+	polybar
+	rofi
+	sxhkd
+	zsh
+)
 
-echo 'Backing up alacritty...'
-if [[ -d ./alacritty ]]; then
-  rm -rf ./alacritty
-fi
-cp -r ~/.config/alacritty .
-
-echo 'Backing up picom...'
-if [[ -d ./picom ]]; then
-  rm -rf ./picom
-fi
-cp -r ~/.config/picom .
-
-echo 'Backing up polybar...'
-if [[ -d ./polybar ]]; then
-  rm -rf ./polybar
-fi
-cp -r ~/.config/polybar .
-
-echo 'Backing up sxhkd...'
-if [[ -d ./sxhkd ]]; then
-  rm -rf ./sxhkd
-fi
-cp -r ~/.config/sxhkd .
-
-echo 'Backing up rofi...'
-if [[ -d ./rofi ]]; then
-  rm -rf ./rofi
-fi
-cp -r ~/.config/rofi .
-
-echo 'Backing up dunst...'
-if [[ -d ./dunst ]]; then
-  rm -rf ./dunst
-fi
-cp -r ~/.config/dunst .
+for config in $config_files
+do
+	echo "Backing up $config..."
+	if [[ -d ./$config ]]; then
+		rm -rf ./$config
+	fi
+	cp -r $HOME/.config/$config .
+done
 
 echo "Config files have been backed up successfully."
--- restore.zsh
+++ restore.zsh
@@ -1,45 +1,23 @@
 #!/bin/zsh
 
-echo 'Installing bspwm...'
-if [[ -d ~/.config/bspwm ]]; then
-  rm -rf ~/.config/bspwm
-fi
-cp -r ./bspwm ~/.config/bspwm
+config_files=(
+	alacritty
+	bspwmm
+	dunst
+	picom
+	polybar
+	rofi
+	sxhkd
+	zsh
+)
 
-echo 'Installing alacritty...'
-if [[ -d ~/.config/alacritty ]]; then
-  rm -rf ~/.config/alacritty
-fi
-cp -r ./alacritty ~/.config/alacritty
+for config in $config_files
+do
+	echo "Restoring $config configs..."
+	if [[ -d $HOME/.config/$config ]]; then
+		rm -rf $HOME/.config/$config
+	fi
+	cp -r ./$config $HOME/.config/$config
+done
 
-echo 'Installing picom...'
-if [[ -d ~/.config/picom ]]; then
-  rm -rf ~/.config/picom
-fi
-cp -r ./picom ~/.config/picom
-
-echo 'Installing polybar...'
-if [[ -d ~/.config/polybar ]]; then
-  rm -rf ~/.config/polybar
-fi
-cp -r ./polybar ~/.config/polybar
-
-echo 'Installing sxhkd...'
-if [[ -d ~/.config/sxhkd ]]; then
-  rm -rf ~/.config/sxhkd
-fi
-cp -r ./sxhkd ~/.config/sxhkd
-
-echo 'Installing rofi...'
-if [[ -d ~/.config/rofi ]]; then
-  rm -rf ~/.config/rofi
-fi
-cp -r ./rofi ~/.config/rofi
-
-echo 'Installing dunst...'
-if [[ -d ~/.config/dunst ]]; then
-  rm -rf ~/.config/dunst
-fi
-cp -r ./dunst ~/.config/dunst
-
-echo "Config files have been installed successfully."
+echo "Config files have been restored successfully."
