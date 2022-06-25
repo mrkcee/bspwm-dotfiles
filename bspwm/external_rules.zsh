a46c12a9c239c71b30a7a0d59e2044e12324bdb3
mrkcee <markz_a@yahoo.com>
2022-06-26 01:09:28 +0800

    Fixed Steam rules

--- bspwm/external_rules.zsh
+++ bspwm/external_rules.zsh
@@ -1,6 +1,24 @@
 #!/bin/zsh
 
 wid="$1"
+# Dialog window
 if xprop -id "$wid" | grep -q _NET_WM_WINDOW_TYPE_DIALOG; then
   echo "state=floating"
+fi
+
+# Steam
+if xprop -id "$wid" | grep -q steam_app_; then
+	echo "state=fullscreen"
+fi
+
+if xprop -id "$wid" | grep -q Settings; then
+	echo "state=floating"
+fi
+
+if xprop -id "$wid" | grep -q "Friends List"; then
+	echo "state=floating"
+fi
+
+if xprop WM_NAME -id "$wid" | grep -q Steam; then
+	echo "state=floating"
 fi
